92219dc8acfef679dd537127ee43f73a9830ff52
KillariDev <killari@gmail.com>
2024-08-02 16:21:30 +0300

    ui added

--- app/ts/components/App.tsx
+++ app/ts/components/App.tsx
@@ -1,46 +1,213 @@
-import { ReadonlySignal } from '@preact/signals'
-import { sleep } from '../library/utilities.js'
-import { useAsyncState } from '../library/preact-utilities.js'
-import { Spinner } from './Spinner.js'
+import { Signal, useSignal } from '@preact/signals'
+import { useEffect, useRef } from 'preact/hooks'
+import { requestAccounts, AccountAddress, DomainInfo, isValidEnsSubDomain, getDomainInfo, getSubstringAfterFirstPoint, doWeNeedToBurnParentFuses, doWeNeedToBurnChildFuses, isChildOwnershipBurned, burnParentFuses, burnChildFuses, transferChildOwnershipAway } from '../utils.js'
+import { namehash } from 'viem'
 
-export interface AppModel {
-	readonly greeting: ReadonlySignal<string>
-	readonly cycleGreeting: () => void
+type CheckBoxes = {
+	parentWrapped: boolean,
+	childWrapped: boolean,
+	parentFusesBurned: boolean,
+	childFusesBurned: boolean,
+	childOwnershipBurned: boolean
+	immutable: boolean
 }
 
-export function App(model: AppModel) {
-	const { value: subject, waitFor: waitForSubject, reset: resetSubject } = useAsyncState<'World'>()
+interface WalletComponentProps {
+	account: Signal<AccountAddress | undefined>
+}
 
-	async function updateSubject() {
-		await sleep(1000)
-		return 'World' as const
+const WalletComponent = ({ account }: WalletComponentProps) => {
+	const connect = async () => {
+		console.log('request')
+		account.value = await requestAccounts()
+		console.log(account.value)
+	}
+	console.log('render', account.value)
+	return account.value !== undefined ? (
+		<p>{ `Connected with ${ account.value }` }</p>
+	) : (
+		<button class = 'button is-primary' onClick = { connect }>
+			{ `Connect wallet` }
+		</button>
+	)
+}
+
+export function App() {
+	const inputValue = useSignal<string>('')
+	const account = useSignal<AccountAddress | undefined>(undefined)
+	const parentDomainInfo = useSignal<DomainInfo | undefined>(undefined)
+	const childDomainInfo = useSignal<DomainInfo | undefined>(undefined)
+	const checkBoxes = useSignal<CheckBoxes | undefined>(undefined)
+	const timeoutRef = useRef<number | null>(null)
+
+	const refreshInformation = () => {
+		console.log('Refreshing information with:', inputValue.value)
+		updateInfos()
 	}
 
-	async function updateSubjectWithFailure(): Promise<'World'> {
-		await sleep(100)
-		throw new Error(`Uh oh, you broke it!`)
+	const updateInfos = async () => {
+		if (!isValidEnsSubDomain(inputValue.value)) return
+
+		const ensSubDomain = inputValue.value
+		const ensParent = getSubstringAfterFirstPoint(ensSubDomain)
+		const [ensLabel] = ensSubDomain.split('.')
+		if (ensLabel === undefined) return
+		if (account.value === undefined) return
+			
+		const childNameHash = namehash(ensSubDomain) 
+		const parentNameHash = namehash(ensParent)
+		const childInfo = await getDomainInfo(account.value, childNameHash, ensParent)
+		const parentInfo = await getDomainInfo(account.value, parentNameHash, inputValue.value)
+		parentDomainInfo.value = parentInfo
+		childDomainInfo.value = childInfo
+
+		checkBoxes.value = {
+			parentWrapped: parentInfo.isWrapped === true,
+			childWrapped: childInfo.isWrapped === true,
+			parentFusesBurned: !doWeNeedToBurnParentFuses(parentInfo),
+			childFusesBurned: !doWeNeedToBurnChildFuses(childInfo, parentInfo),
+			childOwnershipBurned: isChildOwnershipBurned(childInfo),
+			immutable: parentInfo.isWrapped === true && childInfo.isWrapped === true && !doWeNeedToBurnParentFuses(parentInfo) && !doWeNeedToBurnChildFuses(childInfo, parentInfo) && isChildOwnershipBurned(childInfo)
+		}
 	}
 
-	function GoodButton() {
-		return <button style={{ backgroundColor: 'lightgreen' }} onClick={() => waitForSubject(updateSubject)}>Good Button</button>
+	function handleInput(value: string) {
+		inputValue.value = value
+		
+		if (timeoutRef.current !== null) {
+			clearTimeout(timeoutRef.current)
+		}
+
+		timeoutRef.current = window.setTimeout(() => {
+			refreshInformation()
+			timeoutRef.current = null
+		}, 500)
 	}
 
-	function BadButton() {
-		return <button style={{ backgroundColor: 'coral' }} onClick={() => waitForSubject(updateSubjectWithFailure)}>Bad Button</button>
-	}
+	useEffect(() => {
+		return () => {
+			if (timeoutRef.current !== null) {
+				clearTimeout(timeoutRef.current)
+			}
+		}
+	}, [])
+	
+	useEffect(() => { updateInfos() }, [account.value])
 
-	function ResetButton() {
-		return <button onClick={resetSubject}>↻</button>
+	const buttonWrapParent = () => {
+		throw new Error('not implemented')
 	}
+	const buttonWrapChild = () => {
+		throw new Error('not implemented')
 
-	switch (subject.value.state) {
-		case 'inactive':
-			return <main>Waiting for someone to click a button. <GoodButton/><BadButton/></main>
-		case 'pending':
-			return <main><Spinner/> Don't click the bad button! <BadButton/></main>
-		case 'rejected':
-			return <main>{subject.value.error.message} I guess you need to start over, try again: <ResetButton/></main>
-		case 'resolved':
-			return <main><button onClick={model.cycleGreeting}>{model.greeting}</button> {subject.value.value}! <ResetButton/></main>
 	}
+	const buttonBurnParentFuses = async () => {
+		const acc = account.peek()
+		if (acc === undefined) throw new Error('missing account')
+		const parent = parentDomainInfo.peek()
+		if (parent === undefined) throw new Error('parent info missing')
+		await burnParentFuses(acc, parent)
+		await updateInfos()
+	}
+	const buttonBurnChildFuses = async () => {
+		if (!isValidEnsSubDomain(inputValue.value)) return
+		const acc = account.peek()
+		if (acc === undefined) throw new Error('missing account')
+		const parentInfo = parentDomainInfo.peek()
+		if (parentInfo === undefined) throw new Error('parent info missing')
+		const childInfo = childDomainInfo.peek()
+		if (childInfo === undefined) throw new Error('child info missing')
+
+		const ensSubDomain = inputValue.value
+		const [ensLabel] = ensSubDomain.split('.')
+		if (ensLabel === undefined) return
+		if (account.value === undefined) return
+
+		await burnChildFuses(acc, ensLabel, childInfo, parentInfo)
+		await updateInfos()
+	}
+	const buttonBurnChildOwnership = async () => {
+		const acc = account.peek()
+		if (acc === undefined) throw new Error('missing account')
+		const childInfo = childDomainInfo.peek()
+		if (childInfo === undefined) throw new Error('child info missing')
+		await transferChildOwnershipAway(acc, childInfo)
+		await updateInfos()
+	}
+	
+	return <main>
+		<WalletComponent account = { account } />
+		<div style = 'display: block'>
+			<h1>
+				PetalLock
+			</h1>
+			<h2 class = 'subdomain-title'>Make ENS subdomain immutable</h2>
+		</div>
+		{ account.value !== undefined ? <>
+			<input 
+				class = 'input' 
+				type = 'text' 
+				placeholder = 'slasha.vitalik.eth' 
+				value = { inputValue.value } 
+				onInput = { e => handleInput(e.currentTarget.value) }
+			/>
+		</> : <></> }
+		{ childDomainInfo.value === undefined || parentDomainInfo.value === undefined || account.value === undefined? <></> : <>
+			{ checkBoxes.peek()?.immutable ? <p class = 'status' style = 'color: #3cb371'> {`${ childDomainInfo.value.label } IS IMMUTABLE` } </p> : <p class = 'status' style = 'color: #b43c42'> {`${ childDomainInfo.value.label } IS NOT IMMUTABLE` } </p> }
+			{ childDomainInfo.value.isWrapped && BigInt(childDomainInfo.value.owner) !== BigInt(account.value) ? <p style = 'color: #b43c42'>{ `You are not owner of ${ childDomainInfo.value.label }, its owned by ${ childDomainInfo.value.owner }` }</p> : <></> }
+			{ parentDomainInfo.value.isWrapped && BigInt(parentDomainInfo.value.owner) !== BigInt(account.value) ? <p style = 'color: #b43c42'>{ `You are not owner of ${ parentDomainInfo.value.label }, its owned by ${ parentDomainInfo.value.owner }` }</p> : <></> }
+			
+			<div class = 'grid-container'>
+				<div class = 'grid-item'>
+					<label class = 'form-control'>
+						<input type = 'checkbox' checked = { checkBoxes.value?.parentWrapped === true } disabled = { true }/>
+						<p class = 'paragraph checkbox-text'> Parent is wrapped </p>
+					</label>
+				</div>
+				<div class = 'grid-item'>
+					<button class = 'button is-primary' { ...checkBoxes.value?.parentWrapped ? { disabled: true } : {} } onClick = { buttonWrapParent }> Wrap parent </button>
+				</div>
+
+				<div class = 'grid-item'>
+					<label class = 'form-control'>
+						<input type = 'checkbox' checked = { checkBoxes.value?.childWrapped === true } disabled = { true }/>
+						<p class = 'paragraph checkbox-text'> Subdomain is wrapped </p>
+					</label>
+				</div>
+				<div class = 'grid-item'>
+					<button class = 'button is-primary' { ...checkBoxes.value?.childWrapped ? { disabled: true } : {} } onClick = { buttonWrapChild }> Wrap subdomain </button>
+				</div>
+
+				<div class = 'grid-item'>
+					<label class = 'form-control'>
+						<input type = 'checkbox' checked = { checkBoxes.value?.parentFusesBurned === true } disabled = { true }/>
+						<p class = 'paragraph checkbox-text'> Parents fuses are burned </p>
+					</label>
+				</div>
+				<div class = 'grid-item'>
+					<button class = 'button is-primary' { ...checkBoxes.value?.parentFusesBurned ? { disabled: true } : {} } onClick = { buttonBurnParentFuses }> Burn parent fuses </button>
+				</div>
+
+				<div class = 'grid-item'>
+					<label class = 'form-control'>
+						<input type = 'checkbox' checked = { checkBoxes.value?.childFusesBurned === true } disabled = { true }/>
+						<p class = 'paragraph checkbox-text'> Childs fuses are burned </p>
+					</label>
+				</div>
+				<div class = 'grid-item'>
+					<button class = 'button is-primary' { ...checkBoxes.value?.childFusesBurned ? { disabled: true } : {} } onClick = { buttonBurnChildFuses }> Burn child fuses </button>
+				</div>
+
+				<div class = 'grid-item'>
+					<label class = 'form-control'>
+						<input type = 'checkbox' checked = { checkBoxes.value?.childOwnershipBurned === true } disabled = { true }/>
+						<p class = 'paragraph checkbox-text'> Childs ownership is burned </p>
+					</label>
+				</div>
+				<div class = 'grid-item'>
+					<button class = 'button is-primary' { ...(checkBoxes.value?.childOwnershipBurned || !checkBoxes.value?.childFusesBurned || !checkBoxes.value?.childFusesBurned || !checkBoxes.value?.parentWrapped || !checkBoxes.value?.childWrapped) ? { disabled: true } : {} } onClick = { buttonBurnChildOwnership }> Burn ownership to child </button>
+				</div>
+			</div>
+		</> }
+	</main>
 }
---
+++ app/ts/utils.ts
@@ -0,0 +1,234 @@
+import { createWalletClient, custom, keccak256, publicActions, toHex } from 'viem'
+import { mainnet } from 'viem/chains'
+import { ENS_WRAPPER_ABI } from './ens_wrapper_abi.js'
+import { ENS_TOKEN_WRAPPER } from './ens.js'
+import 'viem/window'
+
+export function getSubstringAfterFirstPoint(input: string): string {
+	const pointIndex = input.indexOf('.')
+	if (pointIndex === -1) {
+		return input // Return the original string if no point is found
+	}
+	return input.substring(pointIndex + 1)
+}
+
+type EnsFuseName = 
+  | 'Cannot Unwrap Name'
+  | 'Cannot Burn Fuses'
+  | 'Cannot Transfer'
+  | 'Cannot Set Resolver'
+  | 'Cannot Set Time To Live'
+  | 'Cannot Create Subdomain'
+  | 'Parent Domain Cannot Control'
+  | 'Cannot Approve'
+  | 'Is .eth domain'
+  | 'Can Extend Expiry'
+  | 'Can Do Everything'
+
+type EnsFuseFlag = {
+	name: EnsFuseName
+	value: bigint
+}
+
+// ENS Fuses
+export const CANNOT_UNWRAP = 1n
+export const CANNOT_BURN_FUSES = 2n
+export const CANNOT_TRANSFER = 4n
+export const CANNOT_SET_RESOLVER = 8n
+export const CANNOT_SET_TTL = 16n
+export const CANNOT_CREATE_SUBDOMAIN = 32n
+export const CANNOT_APPROVE = 64n
+export const PARENT_CANNOT_CONTROL = 1n << 16n
+export const IS_DOT_ETH = 1n << 17n
+export const CAN_EXTEND_EXPIRY = 1n << 18n
+export const CAN_DO_EVERYTHING = 0n
+
+const flags: EnsFuseFlag[] = [
+	{ name: 'Cannot Unwrap Name', value: CANNOT_UNWRAP },
+	{ name: 'Cannot Burn Fuses', value: CANNOT_BURN_FUSES },
+	{ name: 'Cannot Transfer', value: CANNOT_TRANSFER },
+	{ name: 'Cannot Set Resolver', value: CANNOT_SET_RESOLVER },
+	{ name: 'Cannot Set Time To Live', value: CANNOT_SET_TTL },
+	{ name: 'Cannot Create Subdomain', value: CANNOT_CREATE_SUBDOMAIN },
+	{ name: 'Cannot Approve', value: CANNOT_APPROVE },
+	{ name: 'Parent Domain Cannot Control', value: PARENT_CANNOT_CONTROL },
+	{ name: 'Is .eth domain', value: IS_DOT_ETH },
+	{ name: 'Can Extend Expiry', value: CAN_EXTEND_EXPIRY },
+	{ name: 'Can Do Everything', value: CAN_DO_EVERYTHING },
+]
+
+export const extractENSFuses = (uint: bigint): readonly EnsFuseName[] => {
+	if (uint === CAN_DO_EVERYTHING) return ['Can Do Everything']
+	const result: EnsFuseName[] = []
+	for (const flag of flags) {
+		if ((uint & flag.value) === flag.value && flag.value !== CAN_DO_EVERYTHING) {
+			result.push(flag.name)
+		}
+	}
+	return result
+}
+
+export const fuseNamesToUint = (names: readonly EnsFuseName[]): number => {
+	let result = 0
+	for (const name of names) {
+		const flag = flags.find(flag => flag.name === name)
+		if (flag) {
+			result |= Number(flag.value)
+		}
+	}
+	return result
+}
+
+export type DomainInfo = {
+	isWrapped: boolean,
+	nameHash: `0x${ string }`
+	owner: `0x${ string }`,
+	data: readonly [`0x${ string }`, number, bigint],
+	fuses: readonly EnsFuseName[],
+	expiry: bigint,
+	label: string,
+}
+
+export type AccountAddress = `0x${ string }`
+
+export const requestAccounts = async () => {
+	if (window.ethereum === undefined) throw new Error('no window.ethereum injected')
+	const reply = await window.ethereum.request({ method: 'eth_requestAccounts', params: undefined })
+	return reply[0]
+}
+
+const createClient = (account: AccountAddress) => {
+	if (window.ethereum === undefined) throw new Error('no window.ethereum injected')
+	if (account === undefined) throw new Error('no account!')
+	return createWalletClient({ 
+		account: account,
+		chain: mainnet, 
+		transport: custom(window.ethereum) 
+	}).extend(publicActions)
+}
+
+export const getDomainInfo = async (account: AccountAddress, nameHash: `0x${ string }`, label: string): Promise<DomainInfo> => {
+	const client = createClient(account)
+	const isWrapped = await client.readContract({
+		address: ENS_TOKEN_WRAPPER,
+		abi: ENS_WRAPPER_ABI, 
+		functionName: 'isWrapped',
+		args: [nameHash]
+	})
+	const owner = await client.readContract({
+		address: ENS_TOKEN_WRAPPER,
+		abi: ENS_WRAPPER_ABI, 
+		functionName: 'ownerOf',
+		args: [BigInt(nameHash)]
+	})
+	
+	const data = await client.readContract({
+		address: ENS_TOKEN_WRAPPER,
+		abi: ENS_WRAPPER_ABI, 
+		functionName: 'getData',
+		args: [BigInt(nameHash)]
+	})
+	const fuses = extractENSFuses(BigInt(data[1]))
+	return {
+		nameHash,
+		isWrapped,
+		owner,
+		data,
+		fuses,
+		expiry: data[2],
+		label
+	}
+}
+
+const parentFuseToBurn = 'Cannot Unwrap Name' as const
+
+export const doWeNeedToBurnParentFuses = (parentInfo: DomainInfo) => {
+	return !parentInfo.fuses.includes(parentFuseToBurn)
+}
+
+export const burnParentFuses = async (account: AccountAddress, parentInfo: DomainInfo) => {
+	if (!doWeNeedToBurnParentFuses(parentInfo)) return undefined
+	const client = createClient(account)
+	const fusesUint = fuseNamesToUint([parentFuseToBurn])
+	const requestHash = await client.writeContract({
+		chain: mainnet, 
+		account,
+		address: ENS_TOKEN_WRAPPER,
+		abi: ENS_WRAPPER_ABI, 
+		functionName: 'setFuses',
+		args: [parentInfo.nameHash, fusesUint]
+	})
+	const receipt = await client.waitForTransactionReceipt({ hash: requestHash })
+	return receipt
+}
+
+const childFusesToBurn = ['Cannot Unwrap Name', 'Cannot Burn Fuses', 'Cannot Set Resolver', 'Cannot Set Time To Live',  'Cannot Create Subdomain', 'Parent Domain Cannot Control', 'Cannot Approve', 'Can Extend Expiry'] as const
+
+export const doWeNeedToBurnChildFuses = (childInfo: DomainInfo, parentInfo: DomainInfo) => {
+	if (fuseNamesToUint(childInfo.fuses) != fuseNamesToUint(childFusesToBurn) && parentInfo.expiry !== childInfo.expiry) {
+		return true 
+	}
+	return false
+}
+
+export const burnChildFuses = async (account: AccountAddress, ensLabel: string, childInfo: DomainInfo, parentInfo: DomainInfo) => {
+	const client = createClient(account)
+	const ensLabelhash = keccak256(toHex(ensLabel))
+	if (doWeNeedToBurnChildFuses(childInfo, parentInfo)) {
+		const requestHash = await client.writeContract({
+			chain: mainnet, 
+			account,
+			address: ENS_TOKEN_WRAPPER,
+			abi: ENS_WRAPPER_ABI, 
+			functionName: 'setChildFuses',
+			args: [parentInfo.nameHash, ensLabelhash, fuseNamesToUint(childFusesToBurn), parentInfo.expiry]
+		})
+		const receipt = await client.waitForTransactionReceipt({ hash: requestHash })
+		return receipt
+	}
+	return undefined
+}
+
+/*
+const wrapDomain = ( domainInfo: DomainInfo) => {
+	if (!domainInfo.isWrapped) {
+		// wrap child
+		throw new Error('child is not wrapped') //TODO, add wrapping
+		const request = await client.writeContract({
+			account,
+			address: ENS_TOKEN_WRAPPER,
+			abi: ENS_WRAPPER_ABI, 
+			functionName: 'wrap',
+			args: [childNameHash, parentNameHash, ENS_RESOLVER]
+		})
+	}
+	return true
+}
+*/
+
+export const isValidEnsSubDomain = (subdomain: string): boolean => {
+	// Regex to validate the ENS subdomain
+	const ensRegex = /^(?!-)[a-zA-Z0-9-]+(?<!-)\.([a-zA-Z0-9-]+\.)?eth$/
+	return ensRegex.test(subdomain)
+}
+
+const burnAddresses = ['0xdeaDDeADDEaDdeaDdEAddEADDEAdDeadDEADDEaD', '0x000000000000000000000000000000000000dEaD', '0x0000000000000000000000000000000000000000'] as const
+
+export const isChildOwnershipBurned = (childInfo: DomainInfo) => {
+	return burnAddresses.map((b) => BigInt(b)).includes(BigInt(childInfo.owner))
+}
+
+export const transferChildOwnershipAway = async (account: AccountAddress, childInfo: DomainInfo) => {
+	if (isChildOwnershipBurned(childInfo)) return undefined
+	const client = createClient(account)
+	const requestHash = await client.writeContract({
+		chain: mainnet,
+		account,
+		address: ENS_TOKEN_WRAPPER,
+		abi: ENS_WRAPPER_ABI, 
+		functionName: 'safeTransferFrom',
+		args: [childInfo.owner, burnAddresses[0], BigInt(childInfo.nameHash), 1n, '0x0']
+	})
+	const receipt = await client.waitForTransactionReceipt({ hash: requestHash })
+	return receipt
+}
