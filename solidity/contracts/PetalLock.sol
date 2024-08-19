91abcae520c9b74b95e7cd4899d4810a13d82313
KillariDev <killari@gmail.com>
2024-08-19 13:28:43 +0300

    contract now functions

--- solidity/contracts/PetalLock.sol
+++ solidity/contracts/PetalLock.sol
@@ -34,10 +34,11 @@ interface IENS_TOKEN_WRAPPER {
 	function getData(uint256 id) external view returns (address, uint32, uint64);
 	function setChildFuses(bytes32 parentNode, bytes32 labelhash, uint32 fuses, uint64 expiry) external;
 	function isWrapped(bytes32 node) external view returns (bool);
+	function setFuses(bytes32 node, uint16 fuses) external returns (uint32);
 }
 
 interface IENS_PUBLIC_RESOLVER {
-	function setContenthash(bytes32 node, bytes memory hash) external;
+	function setContenthash(bytes32 node, bytes calldata hash) external;
 }
 
 interface IENS_ETHEREUM_NAME_SERVICE {
@@ -49,64 +50,74 @@ IENS_TOKEN_WRAPPER constant ens_token_wrapper = IENS_TOKEN_WRAPPER(ENS_TOKEN_WRA
 IENS_ETHEREUM_NAME_SERVICE constant ens_ethereum_name_service = IENS_ETHEREUM_NAME_SERVICE(ENS_ETHEREUM_NAME_SERVICE);
 IENS_PUBLIC_RESOLVER constant ens_public_resolver = IENS_PUBLIC_RESOLVER(ENS_PUBLIC_RESOLVER);
 
-uint32 constant parentFusesToBurn = CANNOT_UNWRAP;
+uint32 constant parentFusesToBurn = CANNOT_UNWRAP + PARENT_CANNOT_CONTROL;
 uint32 constant childFusesToBurn = CANNOT_UNWRAP + CANNOT_BURN_FUSES + CANNOT_SET_RESOLVER + CANNOT_SET_TTL + CANNOT_CREATE_SUBDOMAIN + PARENT_CANNOT_CONTROL + CANNOT_APPROVE + CAN_EXTEND_EXPIRY;
 uint32 constant mandatoryChildFusesToBurn = PARENT_CANNOT_CONTROL;
 address constant burnAddress = 0xdeaDDeADDEaDdeaDdEAddEADDEAdDeadDEADDEaD;
 
-// todo, access controls
 contract PetalLock {
-	// subdomainRoute = ['darkflorist.eth', 'killari.darkflorist.eth', '3.killari.darkflorist.eth']
-	function makeImmutable(string[] memory subdomainRouteNames, string[] memory labels, bytes32[] memory subdomainRouteNodes, bytes32[] memory labelHashes, address[] memory owners, bytes memory contenthash) public {
-		require(subdomainRouteNames.length > 1, 'need to have atleast parent and subdomain');
+	function makeImmutable(address originalOwner, string[] memory labels, bytes32[] memory subdomainRouteNodes, bytes32[] memory labelHashes, bytes memory contenthash) private {
 		require(labelHashes.length == subdomainRouteNodes.length, 'input array length mismatch');
-		require(owners.length == subdomainRouteNodes.length, 'input array length mismatch');
 		require(labelHashes.length == labels.length, 'input array length mismatch');
-		
 		uint256 finalChildIndex = subdomainRouteNodes.length - 1;
 
 		// handle parents
-		for (uint256 i = 0; i < subdomainRouteNames.length; i++) {
+		for (uint256 i = 0; i < labels.length; i++) {
 			bytes32 node = subdomainRouteNodes[i];
 			// 1) check that exists
 			if (!ens_registry.recordExists(node)) {
 				require(i > 0, 'first record need to exist');
-				// create
-				(,, uint64 parentExpiry) = ens_token_wrapper.getData(uint256(subdomainRouteNodes[i]));
+				(,, uint64 parentExpiry) = ens_token_wrapper.getData(uint256(subdomainRouteNodes[i - 1]));
 				bytes32 parentNameHash = subdomainRouteNodes[i - 1];
-				string memory subDomainName = subdomainRouteNames[i];
-				address parentOwner = owners[i - 1];
 				if (i == finalChildIndex) {
-					ens_token_wrapper.setSubnodeRecord(parentNameHash, subDomainName, parentOwner, ENS_PUBLIC_RESOLVER, 0, childFusesToBurn, parentExpiry);
+					ens_token_wrapper.setSubnodeRecord(parentNameHash, labels[i], address(this), ENS_PUBLIC_RESOLVER, 0, 0, parentExpiry);
 				} else {
-					ens_token_wrapper.setSubnodeRecord(parentNameHash, subDomainName, parentOwner, ENS_PUBLIC_RESOLVER, 0, parentFusesToBurn, parentExpiry);
-				}
-			}
-
-			// 2) check that is wrapped
-			if (!ens_token_wrapper.isWrapped(node)) {
-				address parentOwner = owners[i - 1];
-				bytes32 labelhash = labelHashes[i];
-				bytes memory encodedData = abi.encode(labels[i], owners[i], 0, ENS_PUBLIC_RESOLVER);
-				ens_ethereum_name_service.safeTransferFrom(parentOwner, ENS_TOKEN_WRAPPER, uint256(labelhash), encodedData);
-			}
-
-			// 3) check that 'Cannot Unwrap Name' is burned
-			bytes32 nameHash = subdomainRouteNodes[i];
-			(, uint32 newFuses, uint64 newParentExpiry)= ens_token_wrapper.getData(uint256(nameHash));
-			if (i == finalChildIndex) {
-				if (newFuses & mandatoryChildFusesToBurn == mandatoryChildFusesToBurn) {
-					ens_token_wrapper.setChildFuses(subdomainRouteNodes[i - 1], labelHashes[i], childFusesToBurn, newParentExpiry);
+					ens_token_wrapper.setSubnodeRecord(parentNameHash, labels[i], address(this), ENS_PUBLIC_RESOLVER, 0, parentFusesToBurn, parentExpiry);
 				}
 			} else {
-				if (newFuses & parentFusesToBurn == parentFusesToBurn) {
-					ens_token_wrapper.setChildFuses(subdomainRouteNodes[i - 1], labelHashes[i], parentFusesToBurn, newParentExpiry);
+				// 2) check that is wrapped
+				require(ens_token_wrapper.isWrapped(node), 'node not wrapped');
+				// 3) check that 'Cannot Unwrap Name' is burned
+				(, uint32 newFuses,) = ens_token_wrapper.getData(uint256(subdomainRouteNodes[i]));
+				if (i == finalChildIndex) {
+					continue;
+				} else {
+					if (i == 0) {
+						if (newFuses & CANNOT_UNWRAP != CANNOT_UNWRAP) {
+							ens_token_wrapper.setFuses(subdomainRouteNodes[i], CANNOT_UNWRAP);
+						}
+					} else {
+						if (newFuses & parentFusesToBurn != parentFusesToBurn) {
+							(,, uint64 newParentExpiry) = ens_token_wrapper.getData(uint256(subdomainRouteNodes[i - 1]));
+							ens_token_wrapper.setChildFuses(subdomainRouteNodes[i - 1], labelHashes[i], parentFusesToBurn, newParentExpiry);
+						}
+					}
 				}
 			}
 		}
+		
 		ens_public_resolver.setContenthash(subdomainRouteNodes[finalChildIndex], contenthash);
+		(,, uint64 newParentExpiry2) = ens_token_wrapper.getData(uint256(subdomainRouteNodes[finalChildIndex - 1]));
+		ens_token_wrapper.setChildFuses(subdomainRouteNodes[finalChildIndex - 1], labelHashes[finalChildIndex], childFusesToBurn, newParentExpiry2);
 		
 		// burn child owner
-		ens_token_wrapper.safeTransferFrom(owners[finalChildIndex], burnAddress, uint256(subdomainRouteNodes[finalChildIndex]), 1, bytes(''));
+		ens_token_wrapper.safeTransferFrom(address(this), burnAddress, uint256(subdomainRouteNodes[finalChildIndex]), 1, bytes(''));
+		
+		//return tokens to the owner
+		for (uint256 i = 0; i < labels.length - 1; i++) {
+			ens_token_wrapper.safeTransferFrom(address(this), originalOwner, uint256(subdomainRouteNodes[i]), 1, bytes(''));
+		}
 	}
+	
+	function onERC1155Received(address, address from, uint256, uint256, bytes memory data) public virtual returns (bytes4) {
+		// Can we somehow prevent anyone sending to this contract by accident?
+		// currently we need to mint to ourself and this need to return the selector
+		return this.onERC1155Received.selector;
+    }
+
+    function onERC1155BatchReceived(address, address from, uint256[] memory, uint256[] memory, bytes memory data) public virtual returns (bytes4) {
+		(string[] memory labels, bytes32[] memory subdomainRouteNodes, bytes32[] memory labelHashes, bytes memory contenthash) = abi.decode(data, (string[], bytes32[], bytes32[], bytes));
+		makeImmutable(from, labels, subdomainRouteNodes, labelHashes, contenthash);
+	    return this.onERC1155BatchReceived.selector;
+    }
 }
