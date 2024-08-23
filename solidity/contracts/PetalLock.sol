caa4e5ecb91aedfc728b49ad7b28c7729292814d
KillariDev <killari@gmail.com>
2024-08-23 15:25:40 +0300

    two arrays to one array

--- solidity/contracts/PetalLock.sol
+++ solidity/contracts/PetalLock.sol
@@ -48,67 +48,68 @@ uint32 constant FINAL_CHILD_FUSES_TO_BURN = CANNOT_UNWRAP + CANNOT_BURN_FUSES + 
 address constant burnAddress = 0xdeaDDeADDEaDdeaDdEAddEADDEAdDeadDEADDEaD;
 
 // check that tokenId exists in an array
-function exists(uint256 tokenId, bytes32[] memory subdomainRouteNodes) pure returns (bool) {
-	for (uint256 i = 0; i < subdomainRouteNodes.length; i++) {
-		if (uint256(subdomainRouteNodes[i]) == tokenId) return true;
+function exists(uint256 tokenId, SubDomainLabelNode[] memory pathToChild) pure returns (bool) {
+	for (uint256 i = 0; i < pathToChild.length; i++) {
+		if (uint256(pathToChild[i].node) == tokenId) return true;
 	}
 	return false;
 }
 
-contract PetalLock {
-	function makeImmutable(address originalOwner, string[] memory labels, bytes32[] memory subdomainRouteNodes, bytes memory contenthash) private {
-		require(labels.length == subdomainRouteNodes.length, 'input array length mismatch');
-		uint256 finalChildIndex = subdomainRouteNodes.length - 1;
+struct SubDomainLabelNode { 
+	string label;
+	bytes32 node;
+}
 
-		// handle parents
-		for (uint256 i = 0; i < labels.length; i++) {
-			bytes32 node = subdomainRouteNodes[i];
-			// 1) check that the record exists, if not, lets create it
+contract PetalLock {
+	function makeImmutable(address originalOwner, SubDomainLabelNode[] memory pathToChild, bytes memory contenthash) private {
+		uint256 finalChildIndex = pathToChild.length - 1;
+
+		// handle parents, set fuses and create child if needed
+		for (uint256 i = 0; i < pathToChild.length; i++) {
+			bytes32 node = pathToChild[i].node;
+			// check that the record exists, if not, lets create it
 			if (!ensRegistry.recordExists(node)) {
 				require(i > 0, 'first record need to exist');
-				(,, uint64 parentExpiry) = ensTokenWrapper.getData(uint256(subdomainRouteNodes[i - 1]));
-				bytes32 parentNameHash = subdomainRouteNodes[i - 1];
+				(,, uint64 parentExpiry) = ensTokenWrapper.getData(uint256(pathToChild[i - 1].node));
+				bytes32 parentNameHash = pathToChild[i - 1].node;
 				if (i == finalChildIndex) {
-					// do not burn childs fuses here yet as we still need to make modifications to the child
-					ensTokenWrapper.setSubnodeRecord(parentNameHash, labels[i], address(this), ENS_PUBLIC_RESOLVER, 0, 0, parentExpiry);
+					ensTokenWrapper.setSubnodeRecord(parentNameHash, pathToChild[i].label, address(this), ENS_PUBLIC_RESOLVER, 0, FINAL_CHILD_FUSES_TO_BURN, parentExpiry);
 				} else {
-					ensTokenWrapper.setSubnodeRecord(parentNameHash, labels[i], address(this), ENS_PUBLIC_RESOLVER, 0, PARENT_FUSES_TO_BURN, parentExpiry);
+					ensTokenWrapper.setSubnodeRecord(parentNameHash, pathToChild[i].label, address(this), ENS_PUBLIC_RESOLVER, 0, PARENT_FUSES_TO_BURN, parentExpiry);
 				}
 			} else if (i == finalChildIndex) {
-				// if final child is created, the rest of the final child logic is handled later
-				continue;
+				(,, uint64 parentExpiry) = ensTokenWrapper.getData(uint256(pathToChild[i - 1].node));
+				// burn child fuses
+				ensTokenWrapper.setChildFuses(pathToChild[i - 1].node, keccak256(abi.encodePacked(pathToChild[i].label)), FINAL_CHILD_FUSES_TO_BURN, parentExpiry);
 			} else {
-				// 2) the node needs to be wraped
+				// the node needs to be wraped
 				require(ensTokenWrapper.isWrapped(node), 'node not wrapped');
-				// 3) check that 'Cannot Unwrap Name' is burned for top level parent or paren cannot control and cannot wrap is burnt otherwise
-				(, uint32 newFuses,) = ensTokenWrapper.getData(uint256(subdomainRouteNodes[i]));
+				// check that 'Cannot Unwrap Name' is burned for top level parent or paren cannot control and cannot wrap is burnt otherwise
+				(, uint32 newFuses,) = ensTokenWrapper.getData(uint256(pathToChild[i].node));
 				if (i == 0) {
 					if (newFuses & CANNOT_UNWRAP != CANNOT_UNWRAP) {
-						ensTokenWrapper.setFuses(subdomainRouteNodes[i], CANNOT_UNWRAP);
+						ensTokenWrapper.setFuses(pathToChild[i].node, CANNOT_UNWRAP);
 					}
 				} else {
 					if (newFuses & PARENT_FUSES_TO_BURN != PARENT_FUSES_TO_BURN) {
-						(,, uint64 parentExpiry) = ensTokenWrapper.getData(uint256(subdomainRouteNodes[i - 1])); // set childs expiry to be the same as parent, as it cannot be set any longer than that
-						ensTokenWrapper.setChildFuses(subdomainRouteNodes[i - 1], keccak256(abi.encodePacked(labels[i])), PARENT_FUSES_TO_BURN, parentExpiry);
+						(,, uint64 parentExpiry) = ensTokenWrapper.getData(uint256(pathToChild[i - 1].node)); // set childs expiry to be the same as parent, as it cannot be set any longer than that
+						ensTokenWrapper.setChildFuses(pathToChild[i - 1].node, keccak256(abi.encodePacked(pathToChild[i].label)), PARENT_FUSES_TO_BURN, parentExpiry);
 					}
 				}
 			}
 		}
 		
-		ensPublicResolver.setContenthash(subdomainRouteNodes[finalChildIndex], contenthash);
-		(,, uint64 finalChildsParentExpiry) = ensTokenWrapper.getData(uint256(subdomainRouteNodes[finalChildIndex - 1]));
-		
-		// burn child fuses
-		ensTokenWrapper.setChildFuses(subdomainRouteNodes[finalChildIndex - 1], keccak256(abi.encodePacked(labels[finalChildIndex])), FINAL_CHILD_FUSES_TO_BURN, finalChildsParentExpiry);
-		
+		// set content hash
+		ensPublicResolver.setContenthash(pathToChild[finalChildIndex].node, contenthash);
+
 		// burn child owner
-		ensTokenWrapper.safeTransferFrom(address(this), burnAddress, uint256(subdomainRouteNodes[finalChildIndex]), 1, bytes(''));
+		ensTokenWrapper.safeTransferFrom(address(this), burnAddress, uint256(pathToChild[finalChildIndex].node), 1, bytes(''));
 		
 		// return rest of the tokens to the sender
-		uint256[] memory returnableTokens = new uint256[](subdomainRouteNodes.length - 1);
+		uint256[] memory returnableTokens = new uint256[](pathToChild.length - 1);
 		uint256[] memory returnableAmounts = new uint256[](returnableTokens.length);
 		for (uint256 i = 0; i < returnableTokens.length; i++) {
-			returnableTokens[i] = uint256(subdomainRouteNodes[i]);
+			returnableTokens[i] = uint256(pathToChild[i].node);
 			returnableAmounts[i] = 1;
 		}
 		
@@ -124,13 +125,13 @@ contract PetalLock {
 	// when receiving wrapped ens tokens, make the last child immutable
 	function onERC1155BatchReceived(address, address from, uint256[] memory ids, uint256[] memory, bytes memory data) public returns (bytes4) {
 		require(msg.sender == ENS_TOKEN_WRAPPER, 'Supports only Wrapped ENS');
-		(string[] memory labels, bytes32[] memory subdomainRouteNodes, bytes memory contenthash) = abi.decode(data, (string[], bytes32[], bytes));
+		(SubDomainLabelNode[] memory pathToChild, bytes memory contenthash) = abi.decode(data, (SubDomainLabelNode[], bytes));
 		
 		for (uint256 idIndex = 0; idIndex < ids.length; idIndex++) {	
-			require(exists(ids[idIndex], subdomainRouteNodes), 'Sent token does not exist in subdomainRouteNodes');
+			require(exists(ids[idIndex], pathToChild), 'Sent token does not exist in nodes');
 		}
 		
-		makeImmutable(from, labels, subdomainRouteNodes, contenthash);
+		makeImmutable(from, pathToChild, contenthash);
 		return this.onERC1155BatchReceived.selector;
 	}
 }
